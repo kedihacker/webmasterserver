bef739777f313a2a810e391d42cfbbfcdf33f76b
kedi <68711696+kedihacker@users.noreply.github.com>
2022-06-14 13:33:46 +0000

    pls wokr

---
+++ .devcontainer/library-scripts/vgran.sh
@@ -0,0 +1,15 @@
+sudo apt-get install software-properties-common
+apt install wget curl -y
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
+
+sudo apt-get update -y && sudo apt-get install vagrant -y
+
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian buster contrib" >> /etc/apt/sources.list 
+
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+sudo apt-get update -y
+sudo apt-get install qemu virtualbox-6.1 -y
