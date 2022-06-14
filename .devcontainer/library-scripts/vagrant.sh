bdbfdbd7cd9002c88c94c8c65c6eeeead505c730
kedi <68711696+kedihacker@users.noreply.github.com>
2022-06-14 14:09:42 +0000

    help

--- .devcontainer/library-scripts/vagrant.sh
+++ .devcontainer/library-scripts/vagrant.sh
@@ -6,7 +6,7 @@ sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(l
 
 sudo apt-get update -y && sudo apt-get install vagrant -y
 
-echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian buster contrib" >> /etc/apt/sources.list 
+echo "deb [arch=amd64] https://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib" >> /etc/apt/sources.list 
 
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
